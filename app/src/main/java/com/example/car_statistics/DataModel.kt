8f9fc6d20ef60b561eab61953305ad895ea7799e
Большаков Сергей <bolshakov.s@madin.ru>
2022-09-11 17:14:22 +0300

    add DataModel class

---
+++ app/src/main/java/com/example/car_statistics/DataModel.kt
@@ -0,0 +1,10 @@
+package com.example.car_statistics
+public class DataModel() {
+    public abstract class ScannerData {
+        abstract val str: String
+    }
+    public fun Init() {
+        val scannerData: List<ScannerData>
+    }
+}
+
